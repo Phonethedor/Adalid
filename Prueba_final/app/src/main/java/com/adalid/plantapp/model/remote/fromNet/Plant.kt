d3be65398f09b0fa8dbcd00df1533760582d7a43
Alvaro Giovanni Castillo Muñoz <81338935+Phonethedor@users.noreply.github.com>
2023-08-12 03:08:40 -0400

    Delete Prueba_final directory

--- Prueba_final/app/src/main/java/com/adalid/plantapp/model/remote/fromNet/Plant.kt
+++
@@ -1,9 +0,0 @@
-package com.adalid.plantapp.model.remote.fromNet
-
-data class Plant (
-    val id : Int,
-    val name : String,
-    val type : String,
-    val img : String,
-    val desc : String
-        )
